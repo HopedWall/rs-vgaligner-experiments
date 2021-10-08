fd217ca7e7451e3e79f2a2ee4a2e3c51893227a4
HopedWall <francesco.porto97@gmail.com>
2021-10-08 09:58:44 +0200

    Added additional experiments

--- commands.sh
+++ commands.sh
@@ -4,7 +4,6 @@
 cargo install --path ../..
 
 for d in */; do
-	
 	echo "\n"
 	echo "========Executing pipeline for $d======="
 
@@ -26,11 +25,11 @@ for d in */; do
 	vg convert --gam-to-gaf sim.gam sorted_graph.gfa > sim.gaf
 
 	# Run single-thread
-	/usr/bin/time -o aliger_stats_single.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -n 1
-	/usr/bin/time -o mapper_stats_single.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align -n 1
+	#/usr/bin/time -o aliger_stats_single.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -n 1 -r 10
+	#/usr/bin/time -o mapper_stats_single.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align -n 1
 
 	# Run multi-thread
-	/usr/bin/time -o aliger_stats_multi.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11
+	/usr/bin/time -o aliger_stats_multi.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -r 10 
 	/usr/bin/time -o mapper_stats_multi.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align
 
 	# Run gamcompare
@@ -40,6 +39,9 @@ for d in */; do
 	touch graphaligner.gaf
 	GraphAligner -g graph.gfa -f reads.fa -a graphaligner.gaf -x vg
 	python3 ../gafcompare.py graphaligner.gaf sim.gaf > graphaligner_comparison.txt
+
+	# Remove index (it can get quite big)
+	find . -name "*.idx" -type f -delete
 
 	# Move back to main folder
 	cd ..
