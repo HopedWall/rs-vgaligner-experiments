243ddff992b1f18fa2f5e82a12e12930efe3cbcd
HopedWall <francesco.porto97@gmail.com>
2021-10-09 17:58:46 +0200

    Updated commands.sh

--- commands.sh
+++ commands.sh
@@ -25,23 +25,30 @@ for d in */; do
 	vg convert --gam-to-gaf sim.gam sorted_graph.gfa > sim.gaf
 
 	# Run single-thread
-	#/usr/bin/time -o aliger_stats_single.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -n 1 -r 10
-	#/usr/bin/time -o mapper_stats_single.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align -n 1
+	/usr/bin/time -o index_stats_single.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -n 1 -r 10
+	/usr/bin/time -o mapper_stats_single.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align -n 1
 
 	# Run multi-thread
-	/usr/bin/time -o aliger_stats_multi.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -r 10 
+	/usr/bin/time -o index_stats_multi.txt --verbose vgaligner index  -i sorted_graph.gfa -k 11 -r 10 
 	/usr/bin/time -o mapper_stats_multi.txt --verbose vgaligner map -i sorted_graph.idx -f reads.fa --also-align
 
 	# Run gamcompare
 	python3 ../gafcompare.py reads-alignments.gaf sim.gaf > comparison_results.txt
 
+	# Remove vgaligner's index (it can get quite big)
+	find . -name "*.idx" -type f -delete
+
 	# Run graphaligner
 	touch graphaligner.gaf
-	GraphAligner -g graph.gfa -f reads.fa -a graphaligner.gaf -x vg
+	/usr/bin/time -o graphaligner_stats_multi.txt --verbose GraphAligner -g sorted_graph.gfa -f reads.fa -a graphaligner.gaf -x vg
 	python3 ../gafcompare.py graphaligner.gaf sim.gaf > graphaligner_comparison.txt
 
-	# Remove index (it can get quite big)
-	find . -name "*.idx" -type f -delete
+	# Run vg
+	vg view -F sorted_graph.gfa -v > graph.vg
+	/usr/bin/time -o vgind_stats_multi.txt --verbose vg index graph.vg -x graph.xg -g graph.gcsa -k 11
+	/usr/bin/time -o vgmap_stats_multi.txt --verbose vg map -d graph -f single-read-test.fa > vg_result.gam
+	vg convert --gam-to-gaf vg_result.gam sorted_graph.gfa > vg_result.gaf
+	python3 ../gafcompare.py vg_result.gaf sim.gaf > graphaligner_comparison.txt
 
 	# Move back to main folder
 	cd ..
